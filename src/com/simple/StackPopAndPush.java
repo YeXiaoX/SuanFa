94f3ffb5d9984c93818e1b247ec7927f95abbf51
YeXiaoX <497706143@qq.com>
2016-06-16 12:39:59 +0800

    add stackPopAndPush

---
+++ src/com/simple/StackPopAndPush.java
@@ -0,0 +1,59 @@
+package com.simple;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Created by Ivan on 2016/6/16.
+ * 栈的压入和弹出序列
+ * 考察分析复杂问题的能力
+ */
+public class StackPopAndPush {
+    public static boolean isStack(List<Integer> in, List<Integer> out, Stack main) {
+        int count = 0;
+        for (int num = 0; num < in.size(); num++) {
+            main.push(in.get(num));
+            while (!main.isEmpty()&&out.get(count) == main.peek()) {
+                System.out.println(main.pop());
+                count++;
+            }
+        }
+        while (count < out.size()) {
+            if ( !main.isEmpty()&&main.peek() == out.get(count)) {
+                System.out.println(main.pop());
+            }
+            count++;
+        }
+        if (main.isEmpty()) return true;
+        return false;
+
+    }
+
+    public static void pushStack(Stack stack, int in[]) {
+        for (int i = 0; i < in.length; i++) {
+            stack.push(in[i]);
+        }
+    }
+
+    public static void main(String args[]) {
+        List<Integer> in = new ArrayList<>();
+        List<Integer> out = new ArrayList<>();
+        in.add(1);
+        in.add(2);
+        in.add(3);
+        in.add(4);
+        in.add(5);
+        out.add(4);
+        out.add(3);
+        out.add(5);
+        out.add(1);
+        out.add(2);
+        Stack stack = new Stack();
+//        Integer a = in.get(3);
+//        Integer b = out.get(0);
+//        System.out.println(a == b);
+        System.out.println("isStack:" + isStack(in, out, stack));
+
+    }
+}
