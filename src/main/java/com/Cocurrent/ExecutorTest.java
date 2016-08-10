a9cc206bf677fad59dc71da08da1692b08856a89
YeXiaoX <497706143@qq.com>
2016-08-10 21:23:10 +0800

    add executor

--- src/main/java/com/Cocurrent/ExecutorTest.java
+++ src/main/java/com/Cocurrent/ExecutorTest.java
@@ -10,23 +10,34 @@ import java.util.concurrent.*;
  * 把一个任务交给一个线程池去处理，并返回处理结果
  */
 public class ExecutorTest {
-    private final ExecutorService executorService;
+    private final ExecutorService productService;
+    private final ExecutorService consumerService;
+    private final BlockingQueue<String> queue;
 
-    ExecutorTest(ExecutorService executorService) {
-        this.executorService = executorService;
+    ExecutorTest(ExecutorService executorService,BlockingQueue<String> queue,ExecutorService consumerService) {
+        this.productService = executorService;
+        this.queue = queue;
+        this.consumerService = consumerService;
     }
 
     public void exec(String aa[]) {
         List<String> list = new ArrayList<String>();
         Collections.addAll(list, aa);
-        CompletionService<String> completionService = new ExecutorCompletionService<String>(executorService);
+        CompletionService<String> completionService = new ExecutorCompletionService<String>(productService);
+        CompletionService<String> completionService1 = new ExecutorCompletionService<String>(consumerService);
         for (final String s : list) {
             completionService.submit(new Callable<String>() {
                 public String call() throws Exception {
-                    int sum = 0;
-                    for(int i=0;i<1000000;i++)
-                        sum += i;
-                    return s+":"+sum;
+                    queue.add(s);
+                    return s+":"+queue.size();
+                }
+            });
+        }
+
+        while(queue.size()>0){
+            completionService1.submit(new Callable<String>() {
+                public String call() throws Exception {
+                    return queue.take();
                 }
             });
         }
@@ -37,7 +48,7 @@ public class ExecutorTest {
                 String result = f.get();
                 System.out.println(result);
                 if (i == 2) {
-                    Thread.currentThread().interrupt();//这里中断会报异常
+                    //Thread.currentThread().interrupt();//这里中断会报异常,然后consumer将无法执行
                 }
             }
         } catch (InterruptedException e) {
@@ -48,11 +59,26 @@ public class ExecutorTest {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+
+        try {
+            for (int i = 0; i < list.size(); i++) {
+                Future<String> f = completionService1.take();
+                System.out.println(f.cancel(true));//取消正在完成的任务,返回结果为false表示任务已经完成
+                String result = f.get();
+                System.out.println("结果为："+result);
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String args[]) {
         ExecutorService es = Executors.newCachedThreadPool();
-        ExecutorTest e = new ExecutorTest(es);
+        BlockingQueue<String> queue = new ArrayBlockingQueue<String>(4);
+        ExecutorService consumer = Executors.newCachedThreadPool();
+        ExecutorTest e = new ExecutorTest(es,queue,consumer);
         e.exec(new String[]{"1", "2", "3", "4"});
     }
 }
